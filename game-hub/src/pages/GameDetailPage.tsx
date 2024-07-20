85b3d646a0bbf74349bff56b0385fdf84d5b8e23
iliralija <85136877+iliralija@users.noreply.github.com>
2024-07-21 01:09:35 +0200

    Build expandable text

--- game-hub/src/pages/GameDetailPage.tsx
+++ game-hub/src/pages/GameDetailPage.tsx
@@ -1,21 +1,22 @@
-import { useParams } from 'react-router-dom'
-import useGame from '../hooks/useGame';
-import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { Heading, Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
-  const {slug} = useParams();
-  const {data: game, isLoading, error} =  useGame(slug!);
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
 
-  if(isLoading) return <Spinner />;
+  if (isLoading) return <Spinner />;
 
-  if(error || !game) throw error;
+  if (error || !game) throw error;
 
   return (
     <>
-    <Heading>{game.name}</Heading>
-    <Text></Text>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
